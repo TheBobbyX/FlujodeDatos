b80e50424957794956feecca3e3fc7f62c525e2d
AlexisSniffer <alexis.sniffer@gmail.com>
2015-05-16 21:55:50 -0500

    Clase principal
    Se agrega el main en un paquete nuevo

--- src/com/utp/flujodatos/materias/Porcentajes.java
+++ src/com/utp/flujodatos/materias/Porcentajes.java
@@ -5,5 +5,5 @@ public interface Porcentajes {
 	public double porcantajeParciales(double parciales, int porcentaje); 
 	public double porcantajeProyectos(double proyectos, int porcentaje);
 	public double porcantajeSemestral(double semestral, int porcentaje);
-	public double notaFinal(double notafinal);
+	public double notaFinal(double notafinal); 
 }
